e3dc3ff2bb88f68551b762a6aff00015ef1a0cf6
Alessandro Cacciatore <38426513+geronimaw@users.noreply.github.com>
2023-11-15 15:18:39 -0800

    Update __init__.py

--- mdistiller/distillers/__init__.py
+++ mdistiller/distillers/__init__.py
@@ -12,6 +12,7 @@ from .SP import SP
 from .VID import VID
 from .ReviewKD import ReviewKD
 from .DKD import DKD
+from .HROKD import HROKD
 
 distiller_dict = {
     "NONE": Vanilla,
@@ -28,4 +29,5 @@ distiller_dict = {
     "VID": VID,
     "REVIEWKD": ReviewKD,
     "DKD": DKD,
+    "HROKD": HROKD,
 }
