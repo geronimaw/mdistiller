f0f2264eb79026856b28b298b10cd7e220f4cda5
Alessandro Cacciatore <38426513+geronimaw@users.noreply.github.com>
2023-11-15 15:22:30 -0800

    Create __init__.py

---
+++ mdistiller/models/babypose/__init__.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .pose_hrnet import get_pose_net
+from .pose_hrnet import get_pose_net
+from .multi_out_pose_hrnet import get_pose_net
