8a917ff00ab484ef9ab043b42e62b04a551cf2db
Alessandro Cacciatore <38426513+geronimaw@users.noreply.github.com>
2023-11-15 15:21:38 -0800

    Update trainer.py

--- mdistiller/engine/trainer.py
+++ mdistiller/engine/trainer.py
@@ -1,22 +1,24 @@
 import os
 import time
-from tqdm import tqdm
-import torch
+import getpass
 import torch.nn as nn
 import torch.optim as optim
+
+from tqdm import tqdm
 from collections import OrderedDict
-import getpass
 from tensorboardX import SummaryWriter
 from .utils import (
     AverageMeter,
     accuracy,
     validate,
+    validate_hrnet,
     adjust_learning_rate,
     save_checkpoint,
     load_checkpoint,
     log_msg,
 )
 from .dot import DistillationOrientedTrainer
+from mdistiller.engine.losses import JointsMSELoss, JointsKLDLoss
 
 
 class BaseTrainer(object):
@@ -185,6 +187,264 @@ class BaseTrainer(object):
         )
         return msg
 
+
+class HROKD(BaseTrainer):
+    def __init__(self, experiment_name, distiller, train_loader, val_loader, cfg):
+        self.cfg = cfg
+        self.distiller = distiller
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.optimizer = self.init_optimizer(cfg)
+        self.best_acc = -1
+        self.num_outputs = self.cfg.MODEL.N_STAGE if self.cfg.MODEL.MULTI else 1
+        self.criterion_kld = JointsKLDLoss().cuda()
+        self.criterion = JointsMSELoss(use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT).cuda()
+
+        username = getpass.getuser()
+        # init loggers
+        self.log_path = os.path.join(cfg.LOG.PREFIX, experiment_name)
+        if not os.path.exists(self.log_path):
+            os.makedirs(self.log_path)
+        self.tf_writer = SummaryWriter(os.path.join(self.log_path, "train.events"))
+
+    def init_optimizer(self, cfg):
+        if cfg.TRAIN.OPTIMIZER == 'sgd':
+            optimizer = optim.SGD(
+                self.distiller.module.get_learnable_parameters(),
+                lr=cfg.TRAIN.LR,
+                momentum=cfg.TRAIN.MOMENTUM,
+                weight_decay=cfg.TRAIN.WD,
+                nesterov=cfg.TRAIN.NESTEROV
+            )
+        elif cfg.TRAIN.OPTIMIZER == 'adam':
+            optimizer = optim.Adam(
+                self.distiller.module.get_learnable_parameters(),
+                lr=cfg.TRAIN.LR
+            )
+        else:
+            raise NotImplementedError(cfg.TRAIN.OPTIMIZER)
+        return optimizer
+
+    def log(self, lr, epoch, log_dict):
+        # tensorboard log
+        for k, v in log_dict.items():
+            self.tf_writer.add_scalar(k, v, epoch)
+        self.tf_writer.flush()
+        # wandb log
+        if self.cfg.LOG.WANDB:
+            import wandb
+            wandb.log({"current lr": lr})
+            wandb.log(log_dict)
+        if self.get_best_acc(log_dict) > self.best_acc:
+            self.best_acc = self.get_best_acc(log_dict)
+            if self.cfg.LOG.WANDB:
+                wandb.run.summary["best_acc"] = self.best_acc
+        # worklog.txt
+        with open(os.path.join(self.log_path, "worklog.txt"), "a") as writer:
+            lines = [
+                "-" * 25 + os.linesep,
+                "epoch: {}".format(epoch) + os.linesep,
+                "lr: {:.2f}".format(float(lr)) + os.linesep,
+            ]
+            for k, v in log_dict.items():
+                lines.append("{}: {:.2f}".format(k, v) + os.linesep)
+            lines.append("-" * 25 + os.linesep)
+            writer.writelines(lines)
+    
+    def get_best_acc(self, loss_dict):
+        if self.cfg.LOSS.KLD_COUPLES:
+            return loss_dict[f"val_accs_stage{self.cfg.LOSS.KLD_COUPLES[0][0]}"]
+        else:
+            return loss_dict["val_accs_stage1"]
+        
+    def train(self, resume=False):
+        epoch = 1
+        if resume:
+            state = load_checkpoint(os.path.join(self.log_path, "latest"))
+            epoch = state["epoch"] + 1
+            self.distiller.load_state_dict(state["model"])
+            self.optimizer.load_state_dict(state["optimizer"])
+            self.best_acc = state["best_acc"]
+        while epoch < self.cfg.SOLVER.EPOCHS + 1:
+            self.train_epoch(epoch)
+            epoch += 1
+        print(log_msg("Best accuracy:{}".format(self.best_acc), "EVAL"))
+        with open(os.path.join(self.log_path, "worklog.txt"), "a") as writer:
+            writer.write("best_acc\t" + "{:.2f}".format(float(self.best_acc)))
+
+    def train_epoch(self, epoch):
+        lr = adjust_learning_rate(epoch, self.cfg, self.optimizer)
+        train_meters = {
+            "training_time": AverageMeter(),
+            "data_time": AverageMeter(),
+            "losses": AverageMeter(),
+            "hard_losses": AverageMeter(),
+            "soft_losses": AverageMeter(),
+            "teacher_losses": AverageMeter(),
+        }
+        for i in range(self.num_outputs if self.cfg.MODEL.MULTI else 1):
+            train_meters[f"losses_stage{i+1}"] = AverageMeter()
+            train_meters[f"accs_stage{i+1}"] = AverageMeter()
+
+        val_meters = {
+            "val_time": AverageMeter(),
+            "data_time": AverageMeter(),
+            "losses": AverageMeter(),
+            "hard_losses": AverageMeter(),
+            "soft_losses": AverageMeter(),
+            "teacher_losses": AverageMeter(),
+        }
+        for i in range(self.num_outputs if self.cfg.MODEL.MULTI else 1):
+            val_meters[f"losses_stage{i+1}"] = AverageMeter()
+            val_meters[f"accs_stage{i+1}"] = AverageMeter()
+
+        num_iter = len(self.train_loader)
+        pbar = tqdm(range(num_iter))
+
+        # train loops
+        self.distiller.train()
+        for idx, data in enumerate(self.train_loader):
+            msg = self.train_iter(data, epoch, train_meters)
+            pbar.set_description(log_msg(msg, "TRAIN"))
+            pbar.update()
+        pbar.close()
+
+        # validate
+        self.distiller.eval()
+        num_iter = len(self.val_loader)
+        pbar = tqdm(range(num_iter))
+        for idx, data in enumerate(self.val_loader):
+            msg = self.val_iter(data, epoch, val_meters)
+            pbar.set_description(log_msg(msg, "EVAL"))
+            pbar.update()
+        pbar.close()
+
+        log_dict = {}
+        for key, value in train_meters.items():
+            if self.cfg.MODEL.MULTI and any(item in key for item in ['1', '2', '3', '4']):
+                if "4" in key or "3" in key:
+                    log_dict[f"{key}"] = value.avg
+            else:
+                if "hard" not in key and "soft" not in key and "time" not in key:
+                    log_dict[f"{key}"] = value.avg  
+        for key, value in val_meters.items():
+            if self.cfg.MODEL.MULTI and any(item in key for item in ['1', '2', '3', '4']):
+                if "4" in key or "3" in key:
+                    log_dict[f"val_{key}"] = value.avg
+            else:
+                if "hard" not in key and "soft" not in key and "time" not in key:
+                    log_dict[f"val_{key}"] = value.avg  
+
+        self.log(lr, epoch, log_dict)
+        # saving checkpoint
+        state = {
+            "epoch": epoch,
+            "model": self.distiller.state_dict(),
+            "optimizer": self.optimizer.state_dict(),
+            "best_acc": self.best_acc,
+        }
+        student_state = {"model": self.distiller.module.student.state_dict()}
+
+        save_checkpoint(state, os.path.join(self.log_path, "latest"))
+        save_checkpoint(student_state, os.path.join(self.log_path, "student_latest"))
+
+        if epoch % self.cfg.LOG.SAVE_CHECKPOINT_FREQ == 0:
+            save_checkpoint(state, os.path.join(self.log_path, "epoch_{}".format(epoch)))
+            save_checkpoint(
+                student_state, os.path.join(self.log_path, "student_{}".format(epoch)))
+
+        # update the best
+        new_best_acc = self.get_best_acc(log_dict)
+        if not isinstance(new_best_acc, float): 
+            new_best_acc = new_best_acc.val
+        if  new_best_acc >= self.best_acc:
+            save_checkpoint(state, os.path.join(self.log_path, "best"))
+            save_checkpoint(student_state, os.path.join(self.log_path, "student_best"))
+
+    def train_iter(self, data, epoch, train_meters):
+        self.optimizer.zero_grad()
+        train_start_time = time.time()
+        image, target, target_weight, meta = data
+        train_meters["data_time"].update(time.time() - train_start_time)
+        image = image.float()
+        image = image.cuda(non_blocking=True)
+        target = target.cuda(non_blocking=True)
+
+        # forward
+        losses_dict = self.distiller(input=image, target=target, target_weight=target_weight, epoch=epoch)
+
+        # backward
+        loss = losses_dict["loss"]
+        loss.backward()
+        self.optimizer.step()
+        train_meters["training_time"].update(time.time() - train_start_time)
+        # collect info
+        batch_size = image.size(0)
+        train_meters["losses"].update(loss.cpu().detach().numpy().mean(), batch_size)
+        train_meters["hard_losses"].update(losses_dict["hard_loss"], batch_size)
+        train_meters["soft_losses"].update(losses_dict["soft_loss"], batch_size)
+        train_meters["teacher_losses"].update(losses_dict["teacher_loss"], batch_size)
+        for i in range(self.num_outputs if self.cfg.MODEL.MULTI else 1):
+            train_meters[f"losses_stage{i+1}"].update(losses_dict[f"loss_stage{i+1}"], batch_size)
+            train_meters[f"accs_stage{i+1}"].update(losses_dict[f"acc_stage{i+1}"], batch_size)
+        
+        # print info
+        msg = "Epoch:{}| Loss:{:.4f}| Soft_loss:{:.3f}| T_loss:{:.3f}".format(
+                  epoch,
+                  train_meters["training_time"].avg,
+                  train_meters["losses"].avg,
+                  train_meters["soft_losses"].avg,
+                  train_meters["teacher_losses"].avg,
+                  )
+        if self.cfg.LOSS.KLD_COUPLES:
+            for stage in self.cfg.LOSS.KLD_COUPLES[0]:
+                msg += f"| Acc_{stage}stage:{train_meters[f'accs_stage{stage}'].avg:.3f}"
+        else:
+            # if self.cfg.MODEL.MULTI:
+            #     num = self.cfg.MODEL.N_STAGE
+            msg += f"| Acc_1stage:{train_meters[f'accs_stage1'].avg:.3f}"
+        return msg
+
+    def val_iter(self, data, epoch, val_meters):
+        val_start_time = time.time()
+        image, target, target_weight, meta = data
+        val_meters["val_time"].update(time.time() - val_start_time)
+        image = image.float()
+        image = image.cuda(non_blocking=True)
+        target = target.cuda(non_blocking=True)
+
+        # forward
+        # self.cfg, self.val_loader, self.distiller, self.criterion, self.criterion_kld, epoch
+        losses_dict = self.distiller(input=image, target=target, target_weight=target_weight, epoch=epoch)
+
+        # collect info
+        batch_size = image.size(0)
+        loss = losses_dict["loss"]
+        val_meters["losses"].update(loss.cpu().detach().numpy().mean(), batch_size)
+        val_meters["hard_losses"].update(losses_dict["hard_loss"], batch_size)
+        val_meters["soft_losses"].update(losses_dict["soft_loss"], batch_size)
+        val_meters["teacher_losses"].update(losses_dict["teacher_loss"], batch_size)
+        for i in range(self.num_outputs if self.cfg.MODEL.MULTI else 1):
+            val_meters[f"losses_stage{i+1}"].update(losses_dict[f"loss_stage{i+1}"], batch_size)
+            val_meters[f"accs_stage{i+1}"].update(losses_dict[f"acc_stage{i+1}"], batch_size)
+        
+        # print info
+        msg = "Epoch:{}| Loss:{:.4f}| Soft_loss:{:.3f}| T_loss:{:.3f}".format(
+                  epoch,
+                  val_meters["val_time"].avg,
+                  val_meters["losses"].avg,
+                  val_meters["soft_losses"].avg,
+                  val_meters["teacher_losses"].avg,
+                  )
+        if self.cfg.LOSS.KLD_COUPLES:
+            for stage in self.cfg.LOSS.KLD_COUPLES[0]:
+                msg += f"| Acc_{stage}stage:{val_meters[f'accs_stage{stage}'].avg:.3f}"
+        else:
+            # if self.cfg.MODEL.MULTI:
+            #     num = self.cfg.MODEL.N_STAGE
+            msg += f"| Acc_1stage:{val_meters[f'accs_stage1'].avg:.3f}"
+        return msg
+    
 
 class CRDTrainer(BaseTrainer):
     def train_iter(self, data, epoch, train_meters):
